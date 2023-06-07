168c8f901353cb0f97e42daf953161a6acf64354
softwareSouf <braha10bill@gmail.com>
2023-06-06 17:42:03 -0700

    tasks

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-# 6-print_comb3.py
+
 """Print all possible different combinations of two digits in ascending order"""
 for digit1 in range(0, 10):
     for digit2 in range(digit1 + 1, 10):

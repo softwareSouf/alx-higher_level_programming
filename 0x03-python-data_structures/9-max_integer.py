7d4aa826b24faf6b497606dfe03a2429c2b113a7
softwareSouf <braha10bill@gmail.com>
2023-06-13 16:41:02 -0700

    fixing 9

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 
-def divisible_by_2(my_list=None):
-  """Find all multiples of 2 in a list."""
-  if my_list is None:
-    my_list = []
-  multiples = []
-  for i in range(len(my_list)):
-    if my_list[i] % 2 == 0:
-      multiples.append(my_list[i])
-  return multiples
+def max_integer(my_list=[]):
+    if len(my_list) == 0:
+        return (None)
+
+    big = my_list[0]
+    for i in range(len(my_list)):
+        if my_list[i] > big:
+            big = my_list[i]
+
+    return (big)

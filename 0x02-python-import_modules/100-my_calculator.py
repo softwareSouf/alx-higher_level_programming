d1d0886063daf4e18efb6d3da14aa424267bc87c
softwareSouf <braha10bill@gmail.com>
2023-06-14 08:40:46 -0700

    100

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,20 +1,18 @@
-#!/usr/bin/python
+#!/usr/bin/python3
+
 if __name__ == "__main__":
+    from calculator_1 import add, sub, mul, div 
+    import sys
 
     if len(sys.argv) - 1 != 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
 
-    operators = {"+": lambda a, b: a + b,
-                "-": lambda a, b: a - b,
-                "*": lambda a, b: a * b,
-                "/": lambda a, b: a // b}
+    ops = {"+": add, "-": sub, "*": mul, "/": div}
+    if sys.argv[2] not in list(ops.keys()):
+        print("Unknown operator. Available operators: +, -, * and /")
+        sys.exit(1)
 
-    try:
-        a = int(sys.argv[1])
-        b = int(sys.argv[3])
-        operator = sys.argv[2]
-        result = operators[operator](a, b)
-        print("{} {} {} = {}".format(a, operator, b, result))
-    except ValueError:
-    print("Invalid input. Please enter integers for a and b.")
+    a = int(sys.argv[1])
+    b = int(sys.argv[3])
+    print("{} {} {} = {}".format(a, sys.argv[2], b, ops[sys.argv[2]](a, b)))

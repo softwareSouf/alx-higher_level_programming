45505831d5a2a9dec661cd6cdcc76af4a278ab13
softwareSouf <braha10bill@gmail.com>
2023-06-26 16:48:36 -0700

    task0

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,17 +1,12 @@
-import random
+#!/usr/bin/python3
 
 def safe_print_list(my_list=[], x=0):
     ret = 0
     for i in range(x):
-        if i < len(my_list):
+        try:
             print("{}".format(my_list[i]), end="")
             ret += 1
-        else:
+        except IndexError:
             break
     print("")
-    assert ret == x, "Expected {} elements to be printed, but {} were printed".format(x, ret)
     return (ret)
-
-if __name__ == "__main__":
-    my_list = [random.randint(1, 100) for _ in range(10)]
-    print(safe_print_list(my_list, 3))

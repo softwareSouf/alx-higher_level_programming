8e13f1096416bc9307917c86d56fb2ff31ba0511
softwareSouf <braha10bill@gmail.com>
2023-08-15 10:11:29 -0700

    inserting deleting and print the description of the table

---
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -0,0 +1,2 @@
+-- excute the full description of the first_table 
+SHOW CREATE TABLE first_table;
---
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -0,0 +1,2 @@
+-- INSTER NEW ROW IN FIRST_TABLE 
+INSERT INTO first_table(id, name) VALUES (89, 'Best School');

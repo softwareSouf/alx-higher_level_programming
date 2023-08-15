bb351c817416b98cbba3bee28e8ad4164938b854
softwareSouf <braha10bill@gmail.com>
2023-08-15 09:15:36 -0700

    create , select , delete

---
+++ 0x0D-SQL_introduction/0-list_databases.sql
@@ -0,0 +1,2 @@
+-- list databases of the MySQL server are cuurently created
+SHOW DATABASES;
---
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -0,0 +1,4 @@
+-- create table in the database 
+-- if exists the excuting will faild
+CREATE TABLE IF NOT EXISTS first_table (id INT,
+name VARCHAR(256));
